d4260ace71702030ba4662ec6749fbf4d0f253e4
Joel <joeltwaage@gmail.com>
2016-09-20 11:48:06 -0700

    Added Category connection to Items list including CRUD functionality

--- ToDoList/src/ToDoList/Controllers/ItemsController.cs
+++ ToDoList/src/ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ToDoList.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,10 +12,9 @@ namespace ToDoList.Controllers
     public class ItemsController : Controller
     {
         private ToDoListContext db = new ToDoListContext();
-
         public IActionResult Index()
         {
-            return View(db.Items.ToList());
+            return View(db.Items.Include(items => items.Category).ToList());
         }
 
         public IActionResult Details(int id)
@@ -23,41 +23,43 @@ namespace ToDoList.Controllers
             return View(thisItem);
         }
 
-        public IActionResult Create()
+        public ActionResult Create()
         {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Item item)
+        public ActionResult Create(Item item)
         {
             db.Items.Add(item);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public IActionResult Edit(int id)
+        public ActionResult Edit(int id)
         {
             var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
             return View(thisItem);
         }
 
         [HttpPost]
-        public IActionResult Edit(Item item)
+        public ActionResult Edit(Item item)
         {
             db.Entry(item).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int id)
+        public ActionResult Delete(int id)
         {
             var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
             return View(thisItem);
         }
 
         [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(int id)
         {
             var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
             db.Items.Remove(thisItem);
